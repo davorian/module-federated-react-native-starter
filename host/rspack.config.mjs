4d6c5a260ddcc52c3a5cdc876138b3ac67834d78
Fake User <fake@example.com>
2025-06-04 16:35:23 +0100

    5.1.1 repack with react 18.3.1 AND react-navigation

--- host/rspack.config.mjs
+++ host/rspack.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
-import {createRequire} from 'node:module'
-import {composePlugins} from '@nx/rspack'
-import {withZephyr} from 'zephyr-repack-plugin'
-import path from 'node:path'
-import * as Repack from '@callstack/repack'
-import rspack from '@rspack/core'
+import * as Repack from '@callstack/repack';
+// import { getSharedDependencies } from './getSharedDependencies.js';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import rspack from '@rspack/core';
 
 // import pkg from './package.json' with {type: "json"}
 const EXCEPT = ['@module-federation/enhanced']
@@ -18,157 +16,39 @@ const EXCEPT = ['@module-federation/enhanced']
   return Object.fromEntries(shared);
 };*/
 
-// @ts-ignore
 const dirname = Repack.getDirname(import.meta.url)
-// const {resolve} = createRequire(import.meta.url)
+const zephyrDisabled = true
 
-/** @type {(env: import('@callstack/repack').EnvOptions) => import('@rspack/core').Configuration} */
 export default (env) => {
+  //const sharedDepsMobile = JSON.parse(
+  //  readFileSync(join(dirname, '../../shared-deps-mob.json'), 'utf8')
+  //);
+
   const {
-    mode = 'development',
-    context = dirname,
-    entry = './index.js',
-    platform = process.env.PLATFORM,
-    minimize = mode === 'production',
-    devServer = undefined,
-    bundleFilename = undefined,
-    sourceMapFilename = undefined,
-    assetsPath = undefined,
+    platform = process.env.PLATFORM
   } = env
 
-  /**
-   * Using Module Federation might require disabling hmr.
-   * Uncomment below to set `devServer.hmr` to `false`.
-   *
-   * Keep in mind that `devServer` object is not available
-   * when running `webpack-bundle` command. Be sure
-   * to check its value to avoid accessing undefined value,
-   * otherwise an error might occur.
-   */
-  // if (devServer) {
-  //  devServer.hmr = false;
-  // }
-
-  if (!platform) {
-    throw new Error('Missing platform')
-  }
-
-  process.env.BABEL_ENV = mode
+  const defaultRemotes = {
+    "mob_remote1": `mob_remote1@http://localhost:8082/${platform}/mf-manifest.json`,
+    "mob_remote2": `mob_remote2@http://localhost:8083/${platform}/mf-manifest.json`,
+    "mob_remote3": `mob_remote3@http://localhost:8084/${platform}/mf-manifest.json`,
+    "auth": `auth@http://localhost:8083/${platform}/mf-manifest.json`
+  };
 
   return {
-    mode,
-    /**
-     * This should be always `false`, since the Source Map configuration is done
-     * by `SourceMapDevToolPlugin`.
-     */
-    devtool: false,
-    context,
-    entry,
+    context:dirname,
+    entry : './index.js',
     resolve: {
-      /**
-       * `getResolveOptions` returns additional resolution configuration for React Native.
-       * If it's removed, you won't be able to use `<file>.<platform>.<ext>` (eg: `file.ios.js`)
-       * convention and some 3rd-party libraries that specify `react-native` field
-       * in their `package.json` might not work correctly.
-       */
-      ...Repack.getResolveOptions(platform),
-      /**
-       * Uncomment this to ensure all `react-native*` imports will resolve to the same React Native
-       * dependency. You might need it when using workspaces/monorepos or unconventional project
-       * structure. For simple/typical project you won't need it.
-       */
-      // alias: {
-      //   'react-native': reactNativePath,
-      // },
-    },
-    externalsType: 'module', // Use ESM instead of commonjs
-    /**
-     * Configures output.
-     * It's recommended to leave it as it is unless you know what you're doing.
-     * By default, Webpack will emit files into the directory specified under `path`. In order for the
-     * React Native app use them when bundling the `.ipa`/`.apk`, they need to be copied over with
-     * `Repack.OutputPlugin`, which is configured by default inside `Repack.RepackPlugin`.
-     */
-    output: {
-      clean: true,
-      hashFunction: 'xxhash64',
-      path: path.join(dirname, 'build', 'host', platform),
-      filename: 'index.bundle',
-      chunkFilename: '[name].chunk.bundle',
-      publicPath: Repack.getPublicPath({ platform, devServer }),
-      uniqueName: 'host',
-    },
-    /** Configures optimization of the built bundle. */
-    optimization: {
-      /** Enables minification based on values passed from React Native Community CLI or from fallback. */
-      minimize,
-      /** Configure minimizer to process the bundle. */
-      chunkIds: 'named',
+      ...Repack.getResolveOptions(),
     },
     module: {
       rules: [
-        Repack.REACT_NATIVE_LOADING_RULES,
-        Repack.NODE_MODULES_LOADING_RULES,
-        Repack.FLOW_TYPED_MODULES_LOADING_RULES,
-        {
-          test: /\.[jt]sx?$/,
-          type: 'javascript/auto',
-          exclude: /node_modules\/(?!@react-native-masked-view)/, // Exclude all but this package
-          use: {
-            loader: 'builtin:swc-loader',
-            options: {
-              env: {
-                targets: { 'react-native': '0.77' },
-              },
-              jsc: {
-                assumptions: {
-                  setPublicClassFields: true,
-                  privateFieldsAsProperties: true,
-                },
-                externalHelpers: true,
-                transform: {
-                  react: {
-                    runtime: 'automatic',
-                  },
-                },
-              },
-            },
-          },
-        },
-        {
-          test: Repack.getAssetExtensionsRegExp(Repack.ASSET_EXTENSIONS),
-          use: {
-            loader: '@callstack/repack/assets-loader',
-            options: {
-              platform,
-              devServerEnabled: Boolean(devServer),
-              inline: true,
-            },
-          },
-        },
+        ...Repack.getJsTransformRules(),
+        ...Repack.getAssetTransformRules(),
       ],
     },
     plugins: [
-      /**
-       * Configure other required and additional plugins to make the bundle
-       * work in React Native and provide good development experience with
-       * sensible defaults.
-       *
-       * `Repack.RepackPlugin` provides some degree of customization, but if you
-       * need more control, you can replace `Repack.RepackPlugin` with plugins
-       * from `Repack.plugins`.
-       */
-      new Repack.RepackPlugin({
-        context,
-        mode,
-        platform,
-        devServer,
-        output: {
-          bundleFilename,
-          sourceMapFilename,
-          assetsPath,
-        },
-      }),
+      new Repack.RepackPlugin(),
       new Repack.plugins.ModuleFederationPluginV2({
         name: 'host',
         filename: 'host.container.js.bundle',
@@ -178,8 +58,8 @@ export default (env) => {
         remotes: {
           mfeFood: `mfeFood@http://localhost:8082/${platform}/mf-manifest.json`,
           auth: `auth@http://localhost:8083/${platform}/mf-manifest.json`,
-          // deepLinkDynNav: `deepLinkDynNav@http://localhost:8084/${platform}/mf-manifest.json`,
-          deepLinkDynNav: `deepLinkDynNav@https://t-ios-latest-lyndon-fasanya-deep-link-dyn-nav-module--bba357-ze.zephyrcloud.app/deepLinkDynNav.container.js.bundle`,
+         deepLinkDynNav: `deepLinkDynNav@http://localhost:8084/${platform}/mf-manifest.json`,
+           //deepLinkDynNav: `deepLinkDynNav@https://t-ios-latest-lyndon-fasanya-deep-link-dyn-nav-module--bba357-ze.zephyrcloud.app/deepLinkDynNav.container.js.bundle`,
         },
         dts: false,
         shared: {
@@ -230,5 +110,5 @@ export default (env) => {
         resourceRegExp: /^@react-native-masked-view/,
       }),
     ],
-  }
-}
+  };
+};
